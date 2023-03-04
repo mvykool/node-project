5cb7edfb95ab1cc815531bb0c422819ababbc27d
mvykool <maykolandres4@gmail.com>
2023-03-04 18:25:25 -0500

    added testing routes

---
+++ routes/contactRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").get ((req, res) => {
+    res.status(200).json({message: "get all contacts"});
+});
+
+router.route("/").post ((req, res) => {
+    res.status(200).json({message: "create all contacts"});
+});
+
+router.route("/:id").get ((req, res) => {
+    res.status(200).json({message: `Get contact for ${req.params.id}`});
+});
+
+
+router.route("/:id").put ((req, res) => {
+    res.status(200).json({message: `Update contact for ${req.params.id}`});
+});
+
+router.route("/:id").delete ((req, res) => {
+    res.status(200).json({message:`Delete contact for ${req.params.id}`});
+});
+
+module.exports = router;
